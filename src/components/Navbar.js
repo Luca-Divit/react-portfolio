e813d48a06547d129c57f8c63b53949609a87533
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-04-09 13:32:32 +0100

    removed resume button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,8 +19,7 @@ const Navbar = (props) => {
     <div className="flex justify-between py-5 px-10 items-center fixed w-full bg-white z-20 dark:bg-slate-700 dark:text-white">
       <h1 className="cursor-pointer font-dancing text-xl hover:drop-shadow-lg shadow-slate-500" style={{opacity: opacity}}><a href="#">Luca Di Vittorio</a></h1>
       <ul className="flex items-center">
-        <li className="cursor-pointer text-xl opacity-70 hover:opacity-100 hover:drop-shadow-xl shadow-slate-500">{ props.darkMode ? <MdOutlineLightMode onClick={props.onClick}/> : <GiNightSleep onClick={props.onClick}/> }</li>
-        <li className="text-lg ml-10 py-1 px-3 rounded-lg cursor-pointer opacity-90 hover:opacity-100 hover:shadow-md shadow-slate-500 gradient dark:bg-gradient-to-r dark:from-slate-500 dark:to-slate-400"><a href="#">Resume</a></li>
+        <li className="cursor-pointer text-xl opacity-70 hover:opacity-100 hover:drop-shadow-xl shadow-slate-500">{ props.darkMode ? <MdOutlineLightMode size={25} onClick={props.onClick}/> : <GiNightSleep size={25} onClick={props.onClick}/> }</li>
       </ul>
     </div>
   )
