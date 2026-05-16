43681993d856bde4a6f3701487fe908678340eac
LucaDamiano DiVittorio <luca.div@hotmail.com>
2026-05-16 02:04:30 +0100

    Updates

--- src/components/TechPill.js
+++
@@ -1,9 +0,0 @@
-const TechPill = ({ name }) => {
-  return (
-    <div className="rounded-3xl px-3 py-2 bg-[#faebd7] m-1 dark:bg-gradient-to-r dark:from-slate-500 dark:to-slate-400">
-      <p className="text-xs">{name}</p>
-    </div>
-  )
-}
-
-export default TechPill
